66c759e77a0eb9ecdf88d5135170109be5ede4a3
xumepyc-web <de.cheb21@gmail.com>
2023-02-25 12:24:16 +0300

    go5

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 
+
 import NewTaskForm from '../new-task-form/new-task-form';
 import TaskList from '../task-list/task-list';
 import Footer from '../footer/footer';
