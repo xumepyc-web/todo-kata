62d7657b8aac0d464eab42a75785b3f9a2bb6ce0
xumepyc-web <de.cheb21@gmail.com>
2023-03-12 00:54:41 +0300

    todo-timer v0.2

--- src/components/new-task-form/new-task-form.js
+++ src/components/new-task-form/new-task-form.js
@@ -4,11 +4,15 @@ import '../new-task-form/new-task-form.css';
 export default class NewTaskForm extends Component {
   state = {
     label: '',
+    sec: '',
+    min: '',
   };
 
   onLabelChange = (event) => {
+    const name = event.target.name;
+    const target = event.target;
     this.setState({
-      label: event.target.value,
+      [name]: target.value,
     });
   };
 
@@ -17,6 +21,8 @@ export default class NewTaskForm extends Component {
     this.props.addTask(this.state.label);
     this.setState({
       label: '',
+      sec: '',
+      min: '',
     });
   };
 
@@ -25,11 +31,29 @@ export default class NewTaskForm extends Component {
       <form className="new-todo-form" onSubmit={this.onSubmit}>
         <input
           className="new-todo"
+          name="label"
           placeholder="What needs to be done?"
           onChange={this.onLabelChange}
           value={this.state.label}
           autoFocus
         />
+        <input
+          name="min"
+          className="new-todo-form__timer"
+          placeholder="Min"
+          autoFocus
+          onChange={this.onLabelChange}
+          value={this.state.min}
+        />
+        <input
+          name="sec"
+          className="new-todo-form__timer"
+          placeholder="Sec"
+          autoFocus
+          onChange={this.onLabelChange}
+          value={this.state.sec}
+        />
+        <input className="submit" type="submit" />
       </form>
     );
   }
